dd8667571384213889530827ad2f856c62a2cd01
Christopher Debray <christopher.debray@club-employes.com>
2023-03-19 16:37:35 +0100

    ADD inputSelect with dynamic values and options

---
+++ app/src/components/form/InputSelect.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <label :for="inputId">
+      {{ label }}
+      <span v-if="isRequired" style="color: red">*</span>
+    </label>
+    <br />
+    <select 
+      :name="inputId"
+      :id="inputId"
+      v-model="input"
+      @change="$emit('update:modelValue', $event.target.value)"
+    >
+      <option value=""></option>
+      <option :value="option.value" v-for="option in options" :key="option.value">{{ option.label }}</option>
+    </select>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps, PropType, ref } from "vue";
+
+interface IOption {
+  value: string;
+  label: string;
+}
+
+const input = ref("");
+
+defineProps({
+  inputId: {
+    type: String,
+    required: true,
+  },
+  label: {
+    type: String,
+    required: true,
+  },
+  isRequired: {
+    type: Boolean,
+    default: false,
+  },
+  options: {
+    type: Object as PropType<IOption>,
+    required: true,
+  },
+});
+</script>
+
+<style></style>
--- app/src/components/todolist/TodoListForm.vue
+++ app/src/components/todolist/TodoListForm.vue
@@ -14,6 +14,13 @@
       :is-required="true"
       v-model="contentInputValue"
     />
+    <InputSelect
+      :input-id="'task_status'"
+      :label="'Status of the task'"
+      :is-required="true"
+      :options="taskStatus"
+      v-model="statusSelectValue"
+    />
     <button @click.prevent="onSubmit">Enregistrer</button>
   </form>
 </template>
@@ -21,14 +28,36 @@
 <script setup lang="ts">
 import { ref } from "vue";
 import InputText from "../form/InputText.vue";
+import InputSelect from "../form/InputSelect.vue";
 
 const titleInputValue = ref("");
 const contentInputValue = ref("");
+const statusSelectValue = ref("");
+
+const taskStatus = [
+  {
+    value: "active",
+    label: "Active",
+  },
+  {
+    value: "inactive",
+    label: "Inactive",
+  },
+  {
+    value: "done",
+    label: "Done",
+  },
+  {
+    value: "cancelled",
+    label: "Cancelled",
+  },
+];
 
 function onSubmit() {
   console.log("Submitted");
   console.log(titleInputValue);
   console.log(contentInputValue);
+  console.log(statusSelectValue);
 }
 </script>
 
